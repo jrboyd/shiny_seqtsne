9eb62c11ec20c7fba5864eb5af8b6f5ca7e4ac24
jrboyd <jrboyd@med.uvm.edu>
2019-04-19 15:36:06 -0400

    zoom works genes next

--- server.R
+++ server.R
@@ -7,71 +7,100 @@
 #    http://shiny.rstudio.com/
 #
 
-
-
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
     output$globalPlot <- renderPlot({
         typ = input$globalViewType
+        point_size = .1
+        xrng = plot_zoom_xrng()
+        yrng = plot_zoom_yrng()
         if(typ == GLOBAL_VIEW_POINTS){
-            p = ggplot(tsne_tp, aes(x = tx, y = ty)) +
-                geom_point()
+            if(length(input$selMarks) >= 1){
+                if(length(input$selMarks) == 2){
+                    corr_dt = dcast(
+                        agg_dt[cell %in% input$selCells & mark %in% input$selMarks], 
+                        id+cell+tx+ty~mark, value.var = "value")
+                    corr_dt$diff = corr_dt[, 6] - corr_dt[,5]
+                    p = ggplot(corr_dt, aes(x = tx, y = ty, color = diff)) +
+                        geom_point(size = point_size) + 
+                        scale_color_gradientn(colors = c("blue", "white", "red"), 
+                                              breaks = c(-1, -.5, 0, .5, 1),
+                                              labels = c(colnames(corr_dt)[5], "", "-", "", colnames(corr_dt)[6]),
+                                              limits = c(-1, 1)) +
+                        coord_cartesian(xlim = xrng, ylim = yrng) +
+                        labs(color = "difference")
+                }else{
+                    p = ggplot(
+                        agg_dt[cell %in% input$selCells & mark %in% input$selMarks], 
+                        aes(x = tx, y = ty, color = value)) +
+                        geom_point(size = point_size) +  
+                        coord_cartesian(xlim = xrng, ylim = yrng) +
+                        facet_wrap("mark")    
+                }
+            }else{
+                p = ggplot(tsne_dt[cell %in% input$selCells], aes(x = tx, y = ty)) +
+                    coord_cartesian(xlim = xrng, ylim = yrng) +
+                    geom_point(size = point_size)
+            }
+            
         }else if(typ == GLOBAL_VIEW_DENSITY){
-            p = ggplot(tsne_dt, aes(x = tx, y = ty)) +
-                geom_density2d()
+            p = ggplot(tsne_dt[cell %in% input$selCells], aes(x = tx, y = ty)) +
+                coord_cartesian(xlim = xrng, ylim = yrng) +
+                geom_density2d() + 
+                facet_wrap("cell") 
         }else if(typ == GLOBAL_VIEW_PROFILES_FAST){
-            p = stsPlotSummaryProfiles(profile_dt, tsne_dt, x_points = 8)
+            p = stsPlotSummaryProfiles(profile_dt,
+                                       tsne_dt, 
+                                       q_cells = input$selCells,
+                                       q_marks = input$selMarks,
+                                       x_points = input$numBins,
+                                       xrng = xrng,
+                                       yrng = yrng,
+                                       line_color_mapping = color_mapping
+                                        )
+            p
         }else if(typ == GLOBAL_VIEW_PROFILES_SLOW){
-            # n_points = 10
-            # piles_img_res = make_tsne_img(profiles_dt = profile_dt,
-            #                               position_dt = tsne_dt, #force_rewrite = TRUE,
-            #                               apply_norm = FALSE,
-            #                               ylim = c(0,10),
-            #                               # xrng = zoom_x,
-            #                               # yrng = zoom_y,
-            #                               n_points = n_points,
-            #                               line_colors = c(
-            #                                   "H3K4me3" = "forestgreen",
-            #                                   "H3K27me3" = "firebrick")
-            # )
-            # 
-            # p_basic = make_img_plots(img_results = list(piles_img_res),
-            #                          qcell = NULL,
-            #                          min_size = 0,
-            #                          N_ceiling = NULL,
-            #                          as_facet = FALSE)[[1]]
-            # p = p_basic
-            p = stsPlotSummaryProfiles(profile_dt, tsne_dt, x_points = 8, plot_type = "raster")
+            p = stsPlotSummaryProfiles(profile_dt, tsne_dt, 
+                                       q_cells = input$selCells,
+                                       q_marks = input$selMarks,
+                                       x_points = input$numBins, 
+                                       xrng = xrng,
+                                       yrng = yrng,
+                                       line_color_mapping = color_mapping,
+                                       plot_type = "raster")
         }
-
-        if(any(input$xrng != c(-.5, .5)) | any(input$yrng != c(-.5, .5))){
-            p = p + annotate("rect",
-                             xmin = min(input$xrng), xmax = max(input$xrng),
-                             ymin = min(input$yrng), ymax = max(input$yrng),
-                             fill = "#00FF0055", color = "black")
-        }
+        
+        # if(any(input$xrng != c(-.5, .5)) | any(input$yrng != c(-.5, .5))){
+        #     p = p + annotate("rect",
+        #                      xmin = min(input$xrng), xmax = max(input$xrng),
+        #                      ymin = min(input$yrng), ymax = max(input$yrng),
+        #                      fill = "#00FF0055", color = "black")
+        # }
         p +
-            coord_fixed() + theme_classic() + labs(x = "", y = "")
-
+            # coord_fixed() + 
+            theme_classic() + 
+            labs(x = "", y = "")
+        
     })
-
+    
     output$zoomPlot <- renderPlot({
-        zimg_res = make_tsne_img(profile_dt[cell %in%  input$selCells],
-                                 tsne_dt, n_points = input$bins,
-                                 xrng = zoom_xrng(), yrng = zoom_yrng())
-        p = make_img_plots(zimg_res, min_size = .3, qcell = input$selCells,
-                           xrng = zoom_xrng(),
-                           yrng = zoom_yrng(),
-                           as_facet = TRUE ) +
-            theme_classic()
-        p
+        ggplot()
+        # zimg_res = make_tsne_img(profile_dt[cell %in%  input$selCells],
+        #                          tsne_dt, n_points = input$bins,
+        #                          xrng = sel_zoom_xrng(), yrng = sel_zoom_yrng())
+        # p = make_img_plots(zimg_res, min_size = .3, qcell = input$selCells,
+        #                    xrng = sel_zoom_xrng(),
+        #                    yrng = sel_zoom_yrng(),
+        #                    as_facet = TRUE ) +
+        #     theme_classic()
+        # p
     })
-
+    
     output$imgPlot <- renderPlot({
         p_basic +
             coord_fixed() + theme_classic() + labs(x = "", y = "")
     })
-
+    
     output$genePlot <- renderPlot({
         plot_velocity_arrows_selected(tsne_dt,
                                       query_gr,
@@ -79,7 +108,7 @@ shinyServer(function(input, output) {
                                       tss_ids = input$selGenes) +
             coord_fixed() + theme_classic() + labs(x = "", y = "")
     })
-
+    
     output$profilePlot <- renderPlot({
         plot_profiles_selected(profile_dt,
                                query_gr,
@@ -87,7 +116,56 @@ shinyServer(function(input, output) {
                                tss_ids = input$selGenes) +
             theme_classic() + labs(x = "", y = "")
     })
-
+    
+    output$globalDebug = renderText({
+        if(!is.null(input$global_click) | !is.null(input$global_brush)){
+            # browser()
+        }
+        msg_head = "globalPlot interaction:"
+        if(!is.null(input$global_click)){
+            click_val = lapply(input$global_click[c("x", "y")], round, digits = 3)
+            msg_click = paste0("  Click:", click_val$x, ", ", click_val$y)
+        }else{
+            msg_click = "  Click: -"
+        }
+        if(!is.null(input$global_brush)){
+            brush_val = lapply(input$global_brush[c("xmin", "xmax", "ymin", "ymax")], round, digits = 3)
+            msg_brush = paste0("  Brush:", brush_val$xmin, " to ", brush_val$xmax, 
+                               ", ", brush_val$ymin, " to ", brush_val$ymax)
+        }else{
+            msg_brush = "  Brush: -"
+        }
+        if(!is.null(sel_zoom_xrng())){
+            msg_xrng = paste0("  select xrng:", paste(round(sel_zoom_xrng(), digits = 3), collapse = " to "))
+        }else{
+            msg_xrng = "  select xrng: -"
+        }
+        if(!is.null(sel_zoom_yrng())){
+            msg_yrng = paste0("  select yrng:", paste(round(sel_zoom_yrng(), digits = 3), collapse = " to "))
+        }else{
+            msg_yrng = "  select yrng: -"
+        }
+        
+        if(!is.null(plot_zoom_xrng())){
+            msg_pxrng = paste0("  plot xrng:", paste(round(plot_zoom_xrng(), digits = 3), collapse = " to "))
+        }else{
+            msg_pxrng = "  plot xrng: -"
+        }
+        if(!is.null(plot_zoom_yrng())){
+            msg_pyrng = paste0("  plot yrng:", paste(round(plot_zoom_yrng(), digits = 3), collapse = " to "))
+        }else{
+            msg_pyrng = "  plot yrng: -"
+        }
+        
+        # tags$span(tags$br(msg_head), tags$br(msg_click), tags$br(msg_brush))
+        paste(sep = "\n",
+              msg_head, msg_click, msg_brush, 
+              msg_xrng, msg_yrng, 
+              msg_pxrng, msg_pyrng
+              )
+        
+    })
+    
     cellPair = reactiveVal(NULL)
     observeEvent({
         input$selCells
@@ -98,10 +176,10 @@ shinyServer(function(input, output) {
                 cellPair(sc)
             }
         }
-
+        
     })
-
-
+    
+    
     output$pairArrows = renderPlot({
         # req(cellPair())
         # cp = cellPair()
@@ -111,7 +189,7 @@ shinyServer(function(input, output) {
         }
         vel_plots[[1]] + theme_classic()
     })
-
+    
     output$pairKey = renderPlot({
         # req(cellPair())
         # cp = cellPair()
@@ -121,25 +199,48 @@ shinyServer(function(input, output) {
         }
         vel_plots[[2]]
     })
-
+    
     # output$pairArrows = renderPlot({
     #
     #         vel_pl
     #     plot_velocity_arrows(tsne_dt, cp[1], cp[2])[[1]] + theme_classic()
     # })
-
-    zoom_xrng = reactiveVal(c(-.5, .5))
-    zoom_yrng = reactiveVal(c(-.5, .5))
-
-    observeEvent(input$doZoom, {
-        req(input$xrng)
-        req(input$yrng)
-        if(any(input$xrng != zoom_xrng())){
-            zoom_xrng(input$xrng)
+    
+    sel_zoom_xrng = reactiveVal(c(-.5, .5))
+    sel_zoom_yrng = reactiveVal(c(-.5, .5))
+    
+    plot_zoom_xrng = reactiveVal(c(-.5, .5))
+    plot_zoom_yrng = reactiveVal(c(-.5, .5))
+    
+    observeEvent(input$global_brush, {
+        if(is.null(input$global_brush)){
+            browser()
+            xrng = c(-.5, .5)
+            yrng = c(-.5, .5)
+        }else{
+            xrng = c(input$global_brush$xmin, input$global_brush$xmax)
+            yrng = c(input$global_brush$ymin, input$global_brush$ymax)    
         }
-        if(any(input$yrng != zoom_yrng())){
-            zoom_yrng(input$yrng)
+        if(any(xrng != sel_zoom_xrng())){
+            sel_zoom_xrng(xrng)
+        }
+        if(any(yrng != sel_zoom_yrng())){
+            sel_zoom_yrng(yrng)
         }
     })
-
+    
+    observeEvent(input$btnZoom, {
+        plot_zoom_xrng(sel_zoom_xrng())
+        plot_zoom_yrng(sel_zoom_yrng())
+    })
+    
+    observeEvent(input$btnReset, {
+        plot_zoom_xrng(c(-.5, .5))
+        plot_zoom_yrng(c(-.5, .5))
+    })
+    
+    
+    output$tableGenes = DT::renderDT(expr = {
+        data.frame(a = 1:3, b = 4:6)
+    })
 })
--- ui.R
+++ ui.R
@@ -12,46 +12,79 @@ source("setup.R")
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-
+    
     # Application title
     titlePanel("Runx1 bookmarking with seqtsne", windowTitle = "seqtsne"),
-
+    
     # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            radioButtons("globalViewType", label = "Global View",
-                         choices = c(GLOBAL_VIEW_POINTS,
-                                     GLOBAL_VIEW_PROFILES_FAST,
-                                     GLOBAL_VIEW_PROFILES_SLOW,
-                                     GLOBAL_VIEW_DENSITY)),
-
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 2,
-                        max = 16,
-                        value = 8),
-            # sliderInput("xrng", "x-range", -.5, .5, value = c(-.5, .5), dragRange = TRUE),
-            # sliderInput("yrng", "y-range", -.5, .5, value = c(-.5, .5), dragRange = TRUE),
-            # actionButton("doZoom", label = "Zoom"),
-            selectInput("selCells", "Select Cells", choices = UI_CELLS, selected = UI_CELLS[1:4], multiple = TRUE),
-            selectInput("selGenes", "Select Genes", choices = UI_GENES, selected = "RUNX1")
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            fluidRow(
-                plotOutput("globalPlot", width = "400px", height = "400px") ,
-                plotOutput("zoomPlot", width = "400px", height = "400px")
-            ),
-            fluidRow(
-                plotOutput("genePlot", width = "400px", height = "400px"),
-                plotOutput("profilePlot", width = "400px", height = "400px")
-            ),
-            fluidRow(
-                plotOutput("pairArrows", width = "400px", height = "400px"),
-                plotOutput("pairKey", width = "400px", height = "400px")
-
-            )
-        )
+    
+    tabsetPanel(tabPanel("Basic Plot",
+                         sidebarLayout(
+                             sidebarPanel(
+                                 fluidRow(
+                                     column(
+                                         width = 4,
+                                         radioButtons(
+                                             "globalViewType",
+                                             label = "Global View",
+                                             choices = c(
+                                                 GLOBAL_VIEW_POINTS,
+                                                 GLOBAL_VIEW_PROFILES_FAST,
+                                                 GLOBAL_VIEW_PROFILES_SLOW,
+                                                 GLOBAL_VIEW_DENSITY
+                                             )
+                                         )
+                                     ),
+                                     column(
+                                         width = 4,
+                                         checkboxGroupInput(
+                                             "selCells",
+                                             "Select Cells",
+                                             choices = UI_CELLS,
+                                             selected = UI_CELLS
+                                         )
+                                     ),
+                                     column(
+                                         width = 4,
+                                         checkboxGroupInput(
+                                             "selMarks",
+                                             "Select Marks",
+                                             choices = UI_MARKS,
+                                             selected = UI_MARKS[1]
+                                         )
+                                     )
+                                 ),
+                                 
+                                 sliderInput("numBins",
+                                             "Number of bins:",
+                                             min = 2,
+                                             max = 16,
+                                             value = 8),
+                                 # selectInput("selGenes", "Select Genes", choices = UI_GENES, selected = "RUNX1"),
+                                 verbatimTextOutput("globalDebug")
+                             ),
+                             
+                             # Show a plot of the generated distribution
+                             mainPanel(
+                                 fluidRow(
+                                     plotOutput("globalPlot", width = "600px", height = "600px",
+                                                brush = brushOpts("global_brush", delay = 500, delayType = "debounce"), click = "global_click"),
+                                     actionButton("btnZoom", "Zoom"),
+                                     actionButton("btnReset", "Reset")
+                                 )#,
+                                 # fluidRow(
+                                 #     plotOutput("genePlot", width = "400px", height = "400px"),
+                                 #     plotOutput("profilePlot", width = "400px", height = "400px")
+                                 # ),
+                                 # fluidRow(
+                                 #     plotOutput("pairArrows", width = "400px", height = "400px"),
+                                 #     plotOutput("pairKey", width = "400px", height = "400px")
+                                 # 
+                                 # )
+                             )
+                         )
+    ), 
+    tabPanel("Gene Table",
+             DT::dataTableOutput("tableGenes"))
     )
 ))
